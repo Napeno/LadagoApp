02f32b648493e4839e79f71634e8d5a8c4a1b879
UIT-21522029 <96015169+Napeno@users.noreply.github.com>
2024-07-14 21:39:12 +0700

    Add homepage UI

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Homepage from './screens/Homepage';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="LOGIN">
+        <Stack.Screen name="LOGIN" component={LoginScreen} />
+        <Stack.Screen name="HOME" component={Homepage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    assets: ['./assets/fonts'],
+    assets: ['./app/assets/fonts'],
   };
   
